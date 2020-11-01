f1d51cda0e951151e9f028f5933bf2149e06c8e0
anilkusc <anilkuscu95@gmail.com>
2020-11-01 15:15:24 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/anilkusc/kuberentes-environment-injector
+
+go 1.15
+
+require (
+	k8s.io/api v0.19.3
+	k8s.io/apimachinery v0.19.3
+)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+func handleMutate(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "%s", err)
+	}
+
+	mutated, err := Mutate(body)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "%s", err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(mutated)
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/mutate", handleMutate)
+
+	s := &http.Server{
+		Addr:           ":8443",
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20, // 1048576
+	}
+
+	log.Fatal(s.ListenAndServeTLS("./ssl/mutateme.pem", "./ssl/mutateme.key"))
+
+}
